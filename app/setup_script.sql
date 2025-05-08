d7861a75f163a492f0f015ff6e88b9624e64e64d
samdouble <samleafs@protonmail.com>
2025-05-07 22:00:17 -0400

    feat: connect to MongoDB

--- app/setup_script.sql
+++ app/setup_script.sql
@@ -11,7 +11,7 @@ create or replace procedure core.test_connection(connection_string string, datab
   packages = ('snowflake-snowpark-python')
   imports = ('/libs/pymongo-4.12.1-cp311-cp311-manylinux_2_17_x86_64.manylinux2014_x86_64.zip', '/procedures/connection.py')
   handler = 'connection.test_connection';
-grant usage on procedure core.test_connection() to application role app_public;
+grant usage on procedure core.test_connection(string, string, string) to application role app_public;
 
 create or replace procedure core.ping()
   returns variant
